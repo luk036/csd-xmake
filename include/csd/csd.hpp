4b3cdde628f40a1ec80bb5b3d9af489685cb4fcb
Wai-Shing Luk <luk036@gmail.com>
2022-10-08 15:19:38 +0800

    variable name at least three characters

--- include/csd/csd.hpp
+++ include/csd/csd.hpp
@@ -7,23 +7,23 @@
 extern auto to_csd(double num, int places) -> std::string;
 extern auto to_csdfixed(double num, unsigned int nnz) -> std::string;
 extern auto longest_repeated_substring(std::string_view sv) -> std::string;
-extern auto to_decimal(std::string_view csd_str) -> double;
+extern auto to_decimal(std::string_view csd) -> double;
 extern auto to_csd_i(int num) -> std::string;
 
 /**
  * @brief Convert the CSD string to a decimal
  *
- * @param csd_str
+ * @param csd
  * @return int
  */
-inline constexpr auto to_decimal_i(std::string_view csd_str) -> int {
+inline constexpr auto to_decimal_i(std::string_view csd) -> int {
     auto num = 0;
-    for (auto&& c : csd_str) {
-        if (c == '0') {
+    for (auto digit : csd) {
+        if (digit == '0') {
             num *= 2;
-        } else if (c == '+') {
+        } else if (digit == '+') {
             num = num * 2 + 1;
-        } else if (c == '-') {
+        } else if (digit == '-') {
             num = num * 2 - 1;
         } else {
             // ignore unknown characters such as '
--- src/csd.cpp
+++ src/csd.cpp
@@ -18,6 +18,7 @@
 #include <iosfwd>       // for string
 #include <string>       // for basic_string
 #include <string_view>  // for string_view
+#include <utility>      // for pair
 
 using std::ceil;
 using std::fabs;
@@ -26,6 +27,7 @@ using std::log2;
 using std::pow;
 using std::string;
 using std::string_view;
+using std::pair;
 
 /**
  * @brief Convert to CSD (Canonical Signed Digit) string representation
@@ -43,39 +45,40 @@ auto to_csd(double num, const int places) -> string {
         return "0";
     }
     auto const absnum = fabs(num);
+    auto temp = int(ceil(log2(absnum * 1.5)));
+    auto [rem, csd] = absnum < 1.0 ? pair{0, string{"0"}} : pair{temp, string{""}};
 
-    int n; // yes, not yet initialized
-    string csd_str; // yes, not yet initialzed
-    if (absnum < 1.0) {
-        n = 0;
-        csd_str = string{"0"};
-    }
-    else {
-        n = int(ceil(log2(absnum * 1.5)));
-        csd_str = string{""};
-    }
-    auto pow2n = pow(2.0, double(n));
-    while (n > -places) {
-        if (n == 0) {
-            csd_str += '.';
+    // int rem; // yes, not yet initialized
+    // string csd; // yes, not yet initialzed
+    // if (absnum < 1.0) {
+    //     rem = 0;
+    //     csd = string{"0"};
+    // }
+    // else {
+    //     rem = int(ceil(log2(absnum * 1.5)));
+    //     csd = string{""};
+    // }
+    auto p2n = pow(2.0, double(rem));
+    for (; rem > -places; --rem) {
+        if (rem == 0) {
+            csd += '.';
         }
-        auto const pow2n_half = pow2n / 2;
-        auto const d = 3.0 * num;
-        if (d > pow2n) {
-            csd_str += '+';
-            num -= pow2n_half;
+        auto const p2n_half = p2n / 2;
+        auto const det = 3.0 * num;
+        if (det > p2n) {
+            csd += '+';
+            num -= p2n_half;
         } else {
-            if (d < -pow2n) {
-                csd_str += '-';
-                num += pow2n_half;
+            if (det < -p2n) {
+                csd += '-';
+                num += p2n_half;
             } else {
-                csd_str += '0';
+                csd += '0';
             }
         }
-        pow2n = pow2n_half;
-        n -= 1;
+        p2n = p2n_half;
     }
-    return csd_str;
+    return csd;
 }
 
 /**
@@ -94,55 +97,54 @@ auto to_csd_i(int num) -> string {
         return "0";
     }
     auto const absnum = abs(num);
-    auto n = int(ceil(log2(absnum * 1.5)));
-    auto csd_str = string{""};
-    auto pow2n = int(pow(2.0, double(n)));
-    while (n > 0) {
-        auto const pow2n_half = pow2n / 2;
-        auto const d = 3 * num;
-        if (d > pow2n) {
-            csd_str += '+';
-            num -= pow2n_half;
-        } else if (d < -pow2n) {
-            csd_str += '-';
-            num += pow2n_half;
+    auto rem = int(ceil(log2(absnum * 1.5)));
+    auto csd = string{""};
+    auto p2n = int(pow(2.0, double(rem)));
+    for (; rem > 0; --rem) {
+        auto const p2n_half = p2n / 2;
+        auto const det = 3 * num;
+        if (det > p2n) {
+            csd += '+';
+            num -= p2n_half;
+        } else if (det < -p2n) {
+            csd += '-';
+            num += p2n_half;
         } else {
-            csd_str += '0';
+            csd += '0';
         }
-        pow2n = pow2n_half;
-        n -= 1;
+        p2n = p2n_half;
     }
-    return csd_str;
+    return csd;
 }
 
 /**
  * @brief Convert the CSD string to a decimal
  *
- * @param csd_str
+ * @param csd
  * @return double
  */
-auto to_decimal(string_view csd_str) -> double {
+auto to_decimal(string_view csd) -> double {
     auto num = 0.0;
     auto loc = 0U;
-    // for (; i < csd_str.size(); ++i) {
-    //     auto c = csd_str[i];
-    auto i = 0U; 
-    for (auto&& c : csd_str) {
-        if (c == '0') {
+    // for (; i < csd.size(); ++i) {
+    //     auto digit = csd[i];
+    auto pos = 0U; 
+    for (auto digit : csd) {
+        if (digit == '0') {
             num *= 2.0;
-        } else if (c == '+') {
+        } else if (digit == '+') {
             num = num * 2.0 + 1.0;
-        } else if (c == '-') {
+        } else if (digit == '-') {
             num = num * 2.0 - 1.0;
-        } else if (c == '.') {
-            loc = i + 1;
+        } else if (digit == '.') {
+            loc = pos + 1;
         } else {
             // ignore unknown characters such as '
         }
-        ++i;
+        ++pos;
     }
     if (loc != 0) {
-        num /= std::pow(2.0, double(csd_str.size() - loc));
+        num /= std::pow(2.0, double(csd.size() - loc));
     }
     return num;
 }
@@ -159,43 +161,35 @@ auto to_csdfixed(double num, unsigned int nnz) -> string {
         return "0";
     }
     auto const absnum = fabs(num);
+    auto temp = int(ceil(log2(absnum * 1.5)));
+    auto [rem, csd] = absnum < 1.0 ? pair{0, string{"0"}} : pair{temp, string{""}};
 
-    int n; // yes, not yet initialized
-    string csd_str; // yes, not yet initialzed
-    if (absnum < 1.0) {
-        n = 0;
-        csd_str = string{"0"};
-    } else {
-        n = int(ceil(log2(absnum * 1.5)));
-        csd_str = string{""};
-    }
-
-    auto pow2n = pow(2.0, double(n));
-    while (n > 0 || (nnz > 0 && fabs(num) > 1e-100)) {
-        if (n == 0) {
-            csd_str += '.';
+    auto p2n = pow(2.0, double(rem));
+    while (rem > 0 || (nnz > 0 && fabs(num) > 1e-100)) {
+        if (rem == 0) {
+            csd += '.';
         }
-        auto const pow2n_half = pow2n / 2;
-        auto const d = 3.0 * num;
-        if (d > pow2n) {
-            csd_str += '+';
-            num -= pow2n_half;
+        auto const p2n_half = p2n / 2;
+        auto const det = 3.0 * num;
+        if (det > p2n) {
+            csd += '+';
+            num -= p2n_half;
             nnz -= 1;
         } else {
-            if (d < -pow2n) {
-                csd_str += '-';
-                num += pow2n_half;
+            if (det < -p2n) {
+                csd += '-';
+                num += p2n_half;
                 nnz -= 1;
             } else {
-                csd_str += '0';
+                csd += '0';
             }
         }
-        pow2n = pow2n_half;
-        n -= 1;
+        p2n = p2n_half;
+        rem -= 1;
 
         if (nnz == 0) {
             num = 0.0;
         }
     }
-    return csd_str;
+    return csd;
 }
